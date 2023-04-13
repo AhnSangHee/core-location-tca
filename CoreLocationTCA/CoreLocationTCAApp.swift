2d6cebf2e9b5da3dc845bf8d4c2556cb032dcb79
Sanghee <dev.sanghee@gmail.com>
2023-04-14 08:42:17 +0900

    Initial commit

---
+++ CoreLocationTCA/CoreLocationTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationTCAApp.swift
+//  CoreLocationTCA
+//
+//  Created by Selina on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
